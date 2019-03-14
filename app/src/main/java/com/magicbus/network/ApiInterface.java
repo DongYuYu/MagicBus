0402a0a8d727edac94f13c80f598c16ac9d2feac
Dong Yu Yu <scratch19@gmail.com>
2019-03-14 12:58:15 +0800

    first init

---
+++ app/src/main/java/com/magicbus/network/ApiInterface.java
@@ -0,0 +1,17 @@
+package com.magicbus.network;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiInterface {
+
+    @GET("/registration.php")
+    Call<String> register();
+
+    @GET("/login.php")
+    Call<LoginResponse> login(); //implemnt LoginResponse pojo
+
+    @GET("/forgot_pass.php")
+    Call<PasswordResponse> forgetPassword();
+
+}
